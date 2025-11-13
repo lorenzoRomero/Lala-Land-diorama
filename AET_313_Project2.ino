91729d3248ffee7cb1071a50a13fffc4c4d51e70
lorenzoRomero <132471059+lorenzoRomero@users.noreply.github.com>
2025-11-13 14:27:32 -0600

    Update AET_313_Project2.ino

--- AET_313_Project2.ino
+++ AET_313_Project2.ino
@@ -1,13 +1,44 @@
-//Testing: Mark's First Commit
+#include <Servo.h>
 
-//change for merge
 
+Servo myservo;  // create Servo object to control a servo
+// twelve Servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+const int buttonPin = 2;
+int buttonState = 0; 
+bool pressedOnce = false;
 void setup() {
-  // put your setup code here, to run once:
-  
+  myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
 }
+
+
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-//I am god
+ 
+buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH && pressedOnce == false) {
+    pressedOnce = true;
+   sweep();
+   Serial.println("button pressed");
+  } else if(buttonState == LOW){
+   pressedOnce = false;
+  }
+ 
 }
+void sweep(){
+
+ for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+}
+

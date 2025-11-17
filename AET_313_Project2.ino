748b9c5b8675894a6e953d8bed557e8324961b8e
Zeste16 <zeste16@outlook.com>
2025-11-17 10:30:27 -0600

    Prep for multiple button activations and servos

--- AET_313_Project2.ino
+++ AET_313_Project2.ino
@@ -1,25 +1,29 @@
+
 #include <Servo.h>
 
-
-Servo myservo;  // create Servo object to control a servo
+Servo myservo_1;  // create Servo object to control a servo
 // twelve Servo objects can be created on most boards
 
 int pos = 0;    // variable to store the servo position
-const int buttonPin = 2;
+
+const int buttonPin_1 = 2;    // individual button pins for each scene
+const int buttonPin_2 = 3;
+const int buttonPin_3 = 4;
+const int buttonPin_4 = 5;
+const int buttonPin_5 = 6;
+
 int buttonState = 0; 
 bool pressedOnce = false;
+
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
+  myservo_1.attach(9);  // attaches the servo on pin 9 to the Servo object
   pinMode(buttonPin, INPUT);
   Serial.begin(9600);
 }
 
-
-
-
 void loop() {
  
-buttonState = digitalRead(buttonPin);
+buttonState = digitalRead(buttonPin_1);
   if (buttonState == HIGH && pressedOnce == false) {
     pressedOnce = true;
    sweep();
@@ -33,12 +37,11 @@ void sweep(){
 
  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo_1.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15 ms for the servo to reach the position
   }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo_1.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15 ms for the servo to reach the position
   }
 }
-
